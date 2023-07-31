9ace7f23f80dd8f3cfacf30c0a5af8a25ef5a82f
agtugchik <agtugchik13@gmail.com>
2023-07-31 13:20:40 +0300

    fix: service name for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,32 +17,32 @@ import { UsersService } from './users.service';
 
 @Controller()
 export class UsersController {
-  constructor(private readonly appService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
   @Get('user')
   getUsers(): Array<IUserWithoutPassword> {
-    return this.appService.getUsers();
+    return this.usersService.getUsers();
   }
 
   @Get('user/:id')
   getUser(@Param('id') id: string): IUserWithoutPassword {
-    return this.appService.getUser(id);
+    return this.usersService.getUser(id);
   }
 
   @UsePipes(new ValidationPipe())
   @Post('user')
   create(@Body() dto: CreateUserDto) {
-    return this.appService.createUser(dto);
+    return this.usersService.createUser(dto);
   }
 
   @Delete('user/:id')
   @HttpCode(204)
   deleteUser(@Param('id') id: string) {
-    this.appService.deleteUser(id);
+    this.usersService.deleteUser(id);
   }
 
   @Put('user/:id')
   updatePassword(@Body() dto: UpdatePasswordDto, @Param('id') id: string) {
-    return this.appService.updatePassword(dto, id);
+    return this.usersService.updatePassword(dto, id);
   }
 }
