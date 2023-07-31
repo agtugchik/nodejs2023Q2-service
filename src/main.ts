7a141283ae65a28385d46d326c630b92062e91da
agtugchik <agtugchik13@gmail.com>
2023-07-31 19:42:30 +0300

    feat: get PORT value from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { config } from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+config();
+
+const PORT = Number(process.env.PORT) || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
